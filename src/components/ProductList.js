5935d838a6637f1d5691e99eaf50e41bca535f80
konka 84 <1784konka@gmail.com>
2021-11-11 20:05:08 +0600

    initial commit

---
+++ src/components/ProductList.js
@@ -0,0 +1,19 @@
+import React from "react";
+export default function ProductList(props){
+
+    
+
+    return( 
+        <div className="flex flex-wrap justify-start items-stretch">
+      
+
+</div>
+
+    )
+
+
+
+
+
+}
+
--- src/router/RouteHandler.js
+++ src/router/RouteHandler.js
@@ -1,12 +1,19 @@
 import React from "react"
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from '../components/home/Home'
+import ProductList from "../components/ProductList"
+//nimport { Navigate } from "react-router"
+//import Navbar from "../components/navbar"
+import Product from "../components/Product"
 
 function RouteHandler() {
     return (
         <Router>
             <Switch>
                 <Route exact path='/' component={Home} />
+            </Switch>
+            <Switch>
+                <Route exact path='/Product' component={Product} />
             </Switch>
         </Router>
     )

dd4ed5242a7cae0c3293ef7926dab2d09b2d9f3d
Akib Uz Zaman <akib.zaman.04@gmail.com>
2021-11-11 17:31:00 +0600

    Revert "added react-icons package"
    This reverts commit 8e8a520b92c365e9f263f33ce86a15cacf049db5.

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,13 +1,10 @@
 import React from 'react'
-import Navbar from '../navbar/Navbar'
-import Search from '../search/Search'
 
 function Home() {
     return (
         <>
-            <Navbar />
-            <div className="flex justify-center mt-4">
-                <Search />
+            <div>
+                Hello
             </div>
         </>
     )
